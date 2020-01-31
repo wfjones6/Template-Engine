1197ae55701884e6230b8fe459977bb5d2482291
wfjones6 <55958934+wfjones6@users.noreply.github.com>
2020-01-31 09:17:42 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const http = require('http');
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require("util");
+const request = require('request');
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
